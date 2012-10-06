18e5257141e217f2d7a90c658ebd538ea5317021
Jonathan Coe <jon@jbcoe.net>
2012-10-06 14:16:43 +0100

    Protein translation solved

--- ProteinTranslation/ProteinTranslation.cpp
+++ ProteinTranslation/ProteinTranslation.cpp
@@ -1,42 +1,76 @@
+#include <stdexcept>                    
 #include <iostream>
+#include <algorithm>
+#include <map>
+#include <iterator>
 
-struct BaseTriple
+int IndexOfBase(char c)
 {
-	char m_b1 = '\0';
-	char m_b2 = '\0';
-	char m_b3 = '\0';
-
-  BaseTriple() {}
-
-	BaseTriple (char b1, char b2, char b3) : 
-		m_b1(b1), m_b2(b2), m_b3(b3) {}
-
-	bool operator < (const BaseTriple& t) const
+	switch(c)
 	{
-		if ( m_b1 < t.m_b1 )
-			return true;
-		if ( t.m_b1 < m_b1 )
-			return false;
-		
-		if ( m_b2 < t.m_b2 )
-			return true;
-		if ( t.m_b2 < m_b2 )
-			return false;
-		
-		if ( m_b3 < t.m_b3 )
-			return true;
-		if ( t.m_b3 < m_b3 )
-			return false;
-
-		return false;
+		case 'U':
+			return 0;
+		case 'C':
+			return 1;
+		case 'A':
+			return 2;
+		case 'G':
+			return 3;
+		default:
+			throw std::runtime_error("Bad base");
 	}
-};
-
-char GetProtein(char b1, char b2, char b3)
-{
-}
-int main(int argc, char* argv[])
-{
 }
 
+const char* const codonToAcid = 
+"FFLL"
+"SSSS"
+"YYss"
+"CCsW"
+"LLLL"
+"PPPP"
+"HHQQ"
+"RRRR"
+"IIIM"
+"TTTT"
+"NNKK"
+"SSRR"
+"VVVV"
+"AAAA"
+"DDEE"
+"GGGG";
+
+int main()
+{
+	std::istream_iterator<char> eos;
+	std::istream_iterator<char> iit (std::cin);
+
+	std::map<char,int> baseCount;
+
+	while ( iit != eos )
+	{
+		char b1 = *iit++;
+		if ( iit == eos )
+			break;
+
+		char b2 = *iit++;
+		if ( iit == eos )
+			break;
+
+		char b3 = *iit++;
+		if ( iit == eos )
+			break;
+
+		char amminoAcid = codonToAcid[
+			16*IndexOfBase(b1) + 
+			4*IndexOfBase(b2) + 
+			IndexOfBase(b3)];
+
+		if ( amminoAcid == 's' )
+			break;
+
+		std::cout << amminoAcid;
+	}
+
+	std::cout << std::endl;
+}
 
